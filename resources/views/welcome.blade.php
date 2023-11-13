3721252375f28c07206351a3f21ce857a6f068b2
paulhenry46 <paulhenry@paulhenry.eu>
2023-11-13 20:47:50 +0100

    Add multiple language support and add french translation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
                     <x-info-message/>
                     <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
                         <div class="sm:col-span-3 col-span-1 h-full ">
-                            <h1 class="text-4xl text-center dark:text-white">Welcome to {{env('APP_NAME')}}</h1>
+                            <h1 class="text-4xl text-center dark:text-white">{{__('Welcome to')}} {{env('APP_NAME')}}</h1>
                             <p class="sm:px-12 text-2xl text-center dark:text-white">{{__('New resources are added throughout the year to support your revision. Here you\'ll find mind maps, summary sheets and flashcards.')}}</p>
                         </div>
 
@@ -17,10 +17,10 @@
                     <div class="col-span-1 col-span-1 h-full ">
                         <div class="card w-full bg-primary text-primary-content h-full">
                             <div class="card-body">
-                              <h2 class="card-title">{{_('News')}}</h2>
-                              <p>{{_('In urgent need of revision? This is where you\'ll find the latest resources.')}}</p>
+                              <h2 class="card-title">{{__('News')}}</h2>
+                              <p>{{__('In urgent need of revision? This is where you\'ll find the latest resources.')}}</p>
                               <div class="card-actions justify-end">
-                                <a wire:navigate href="{{route('post.public.news')}}" class="btn">{{_('See')}}</a>
+                                <a wire:navigate href="{{route('post.public.news')}}" class="btn">{{__('See')}}</a>
                               </div>
                             </div>
                           </div>
@@ -31,7 +31,7 @@
                               <h2 class="card-title">{{__('Library')}}</h2>
                               <p>{{__('Need to consult the resources already added to get a head start or review a concept? Just click here!')}}</p>
                               <div class="card-actions justify-end">
-                                <a wire:navigate href="{{route('post.public.library')}}" class="btn">{{_('See')}}</a>
+                                <a wire:navigate href="{{route('post.public.library')}}" class="btn">{{__('See')}}</a>
                               </div>
                             </div>
                           </div>
@@ -42,7 +42,7 @@
                               <h2 class="card-title">{{__('Participate')}}</h2>
                               <p>{{__('Want to suggest resources? Create an account and let\'s get started!')}}</p>
                               <div class="card-actions justify-end">
-                                <a wire:navigate href="{{route('register')}}" class="btn">{{_('Register')}}</a>
+                                <a wire:navigate href="{{route('register')}}" class="btn">{{__('Register')}}</a>
                               </div>
                             </div>
                           </div>
