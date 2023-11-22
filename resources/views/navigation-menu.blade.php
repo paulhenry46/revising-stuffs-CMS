01970a7e54abf86f9444743dcd5b0bd75fde07ee
paulhenry46 <paulhenrysaux@protonmail.com>
2023-11-22 20:41:14 +0100

    Add 'news' link to layout

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -12,6 +12,9 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                <x-nav-link href="{{ route('post.public.news') }}" :active="request()->routeIs('post.public.news')">
+                        {{ __('News') }}
+                    </x-nav-link>
                     @auth
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
@@ -78,6 +81,9 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
+        <x-responsive-nav-link href="{{ route('post.public.news') }}" :active="request()->routeIs('post.public.news')">
+                        {{ __('News') }}
+                    </x-responsive-nav-link> 
             @auth
                     <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
