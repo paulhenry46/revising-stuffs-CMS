e7dcc3e2800deac42db21722db2ff91608d374aa
paulhenry46 <paulhenrysaux@protonmail.com>
2023-12-26 18:58:12 +0100

    Use Table from daisy UI

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -24,7 +24,82 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <x-user-edit :user=$user/>
+            <div class="p-6 lg:p-8 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700">
+
+<h1 class=" text-2xl font-medium text-gray-900 dark:text-white">
+    @if($user->id !== 0) {{__('Edit an user')}} @else {{__('Add a new user')}} @endif
+</h1>
+
+</div>
+
+<div class="bg-gray-200 dark:bg-gray-800 bg-opacity-25 gap-6 lg:gap-8 p-6 lg:p-8">
+<x-info-message/>
+<form method="POST" action="@if($user->id !== 0) {{route('users.update', $user->id)}} @else {{route('users.store')}} @endif">
+@csrf
+@if($user->id !== 0) @method('put') @endif
+<div class="space-y-12">
+
+<div class="border-b dark:border-white/10 border-gray-900/10 pb-12">
+  <h2 class="text-base font-semibold leading-7 text-gray-900 dark:text-white">{{__('General Informations')}}</h2>
+  <p class="mt-1 text-sm leading-6 text-gray-600 dark:text-white">{{__('This information is used to classify and recognise the account.')}}</p>
+  <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+    <div class="sm:col-span-3">
+      <label for="name" class="block text-sm font-medium leading-6 dark:text-white text-gray-900">{{__('Name')}}</label>
+      <div class="mt-2">
+        <input value="{{ old('name', $user->name) }}" type="text" name="name" id="name" autocomplete="given-name" class="form-input dark:bg-white/5 dark:text-white block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+      </div>
+    </div>
+    @if($user->id == 0)
+    <div class="sm:col-span-3">
+      <label for="email" class="block text-sm font-medium leading-6 dark:text-white text-gray-900">{{__('Email')}}</label>
+      <div class="mt-2">
+        <input value="{{ old('email', $user->email) }}" type="text" name="email" id="email" autocomplete="given-email" class=" form-input dark:bg-white/5 dark:text-white block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+      </div>
+    </div>
+
+    <div class="sm:col-span-3">
+      <label for="password" class="block text-sm font-medium leading-6 dark:text-white text-gray-900">{{__('Password')}}</label>
+      <div class="mt-2">
+        <input value="{{ old('password') }}" type="password" name="password" id="password" class=" form-input dark:bg-white/5 dark:text-white block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+      </div>
+    </div>
+    @endif
+  </div>
+</div>
+<div class="border-b dark:border-white/10 border-gray-900/10 pb-12">
+  <h2 class="text-base font-semibold leading-7 text-gray-900 dark:text-white">{{__('Status')}}</h2>
+  <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+  <div class="sm:col-span-3">
+      <div>
+<label for="role" class="block text-sm font-medium leading-6 dark:text-white text-gray-900">{{__('Role')}}</label>
+<select id="role" name="role" class="form-select mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6 dark:bg-white/5 dark:text-white dark:ring-white/10">
+<option 
+@if((old('role') == 'admin') or ($user->hasExactRoles(['admin', 'moderator', 'contributor', 'student']))) 
+selected 
+@endif 
+value="admin">{{__('Administrator')}}</option>
+
+<option 
+@if((old('role') == 'moderator') or ($user->hasExactRoles(['moderator', 'contributor', 'student']))) 
+selected 
+@endif 
+value="moderator">{{__('Moderator')}}</option>
+
+<option @if((old('role') == 'contributor') or ($user->hasExactRoles(['contributor', 'student']))) selected @endif value="contributor">{{__('Contributor')}}</option>
+
+<option @if((old('role') == 'student') or ($user->hasExactRoles(['student']))) selected @endif value="student">{{__('Student')}}</option>
+</select>
+</div>
+    </div>
+  </div>
+</div>
+<div class="mt-6 flex items-center justify-end gap-x-6">
+<a href="{{route('users.index')}}" class="text-sm font-semibold leading-6 text-red-500">{{__('Cancel')}}</a>
+<button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">@if($user->id !== 0) {{__('Edit')}} @else {{__('Create')}} @endif</button>
+</div>
+</form>
+
+</div>
             </div>
         </div>
     </div>
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -14,7 +14,89 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <x-users-show :users=$users/>
+            <div class="p-6 lg:p-8 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700">
+
+<h1 class=" text-2xl font-medium text-gray-900 dark:text-white">
+    {{__('Manage the users')}}
+</h1>
+
+</div>
+
+<div class="bg-gray-200 dark:bg-gray-800 bg-opacity-25 gap-6 lg:gap-8 p-6 lg:p-8">
+<x-info-message/>
+<div class="px-4 sm:px-6 lg:px-8">
+<div class="sm:flex sm:items-center">
+<div class="sm:flex-auto">
+  <h1 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">{{__('Users')}}</h1>
+  <p class="mt-2 text-sm text-gray-700 dark:text-white">{{__('List of the users')}}</p>
+</div>
+<div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+  <a href="{{route('users.create')}}" class="btn btn-primary">{{__('New user')}}</a>
+</div>
+</div>
+<div class="mt-8 flow-root">
+<div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+  <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+    <table class="table table-zebra">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>{{__('Name')}}</th>
+          <th>{{__('Role')}}</th>
+          <th>{{__('Mail')}}</th>
+          <th>{{__('Email verified')}}</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($users as $user)
+        <tr>
+            <td>{{ $user->id }}</td>
+            <td class="text-gray-100">{{ $user->name }}</td>
+            @if($user->hasExactRoles(['admin', 'moderator', 'contributor', 'student']))
+            <td class="text-red-500">
+              {{__('Administrator')}}
+              @endif
+            @if($user->hasExactRoles(['moderator', 'contributor', 'student']))
+            <td class="text-orange-500">
+              {{__('Moderator')}}
+              @endif
+            @if($user->hasExactRoles(['contributor', 'student']))
+            <td class="text-blue-500">
+              {{__('Contributor')}}
+              @endif
+            @if($user->hasExactRoles(['student']))
+            <td class="text-gray-100">
+              {{__('Student')}}
+              @endif
+            </td>
+            <td>
+              {{$user->email}}</td>
+              @if($user->email_verified_at !== NULL)
+              <td class="text-green-500">
+              <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg></td>
+              @else
+              <td class="text-red-500">
+              <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg></td>
+              @endif
+            <td class="flex items-stretch justify-end relative  text-right">
+                <a href="{{route('users.edit', $user->id)}}" class="text-indigo-600 hover:text-indigo-400"><svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M400-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM80-160v-112q0-33 17-62t47-44q51-26 115-44t141-18h14q6 0 12 2-8 18-13.5 37.5T404-360h-4q-71 0-127.5 18T180-306q-9 5-14.5 14t-5.5 20v32h252q6 21 16 41.5t22 38.5H80Zm560 40-12-60q-12-5-22.5-10.5T584-204l-58 18-40-68 46-40q-2-14-2-26t2-26l-46-40 40-68 58 18q11-8 21.5-13.5T628-460l12-60h80l12 60q12 5 22.5 11t21.5 15l58-20 40 70-46 40q2 12 2 25t-2 25l46 40-40 68-58-18q-11 8-21.5 13.5T732-180l-12 60h-80Zm40-120q33 0 56.5-23.5T760-320q0-33-23.5-56.5T680-400q-33 0-56.5 23.5T600-320q0 33 23.5 56.5T680-240ZM400-560q33 0 56.5-23.5T480-640q0-33-23.5-56.5T400-720q-33 0-56.5 23.5T320-640q0 33 23.5 56.5T400-560Zm0-80Zm12 400Z"/></svg><span class="sr-only">, {{ $user->name }}</span></a>
+  
+                <form action="{{route('users.destroy', $user->id)}}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                <button type="submit" class="ml-4 text-red-600 hover:text-red-300"><svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg><span class="sr-only">, {{ $user->name }}</span></button></form>
+            </td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+    <div class="text-blue-500 text-red-500 text-orange-500 text-green-500 text-yellow-500 text-purple-500  text-pink-500" style="display: none;"></div>
+  </div>
+</div>
+</div>
+</div>
+</div>
             </div>
         </div>
     </div>
