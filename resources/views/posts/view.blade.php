f319aaab60a528518597c64764a16c16c1a5adcb
paulhenry46 <paulhenrysaux@protonmail.com>
2024-03-10 20:51:32 +0100

    Edit post's page

--- resources/views/posts/view.blade.php
+++ resources/views/posts/view.blade.php
@@ -210,50 +210,14 @@
                                         @endif
                                     @endif
                                 @empty
-                                <div class="chat chat-start">
-                                    <div class="chat-image">
-                                        <div class="w-10 bg-error text-error-content rounded-full flex h-10 justify-center items-center">
-                                            <div class="text-center"><p>CS</p></div>
-
-                                        </div>
-                                    </div>
-                                    <div class="chat-header">
-                                        System
-                                        <time class="text-xs opacity-50">{{now()->format('d/m/y')}}</time>
-                                    </div>
-                                    <div class="chat-bubble">{{__('Nobody commented that post.')}}</div>
+                                <div class=" text-lg  pt-6 text-center">
+                                {{__('Nobody commented that post.')}}
                                 </div>
                                 @endforelse
                             </div>
                             <div class="col-span-3 lg:col-span-1">
-                                <div class="card bg-base-100 shadow-xl">
-                                    <div class="card-body">
-                                        <h2 class="card-title">{{__('Identification')}}</h2>
-                                        <h5>{{__('Course')}} : <span class="text-white badge bg-{{$post->course->color}} badge-xm">{{$post->course->name}}</span></h5>
-                                        <h5>{{__('Level')}} : {{$post->level->name}}</h5>
-                                        <h5>{{__('State')}} : 
-                                            @if($post->published)
-                                            <span class="badge badge-success badge-sm">{{__('Published')}}</span>
-                                            @else
-                                            <span class="badge badge-error badge-sm">{{__('Pending')}}</span>
-                                            @endif 
-                                            @if($post->pinned)
-                                            and <span class="badge badge-neutral badge-sm">{{__('Pinned')}}</span></h5>
-                                            @endif
-                                        <h5>{{__('Type')}} : 
-                                            <span class="text-white badge bg-{{$post->type->color}} badge-xm">{{$post->type->name}}</span>
-                                        </h5>
-                                        <h5>{{__('Date')}} : {{$post->created_at->format('d/m/Y')}}</h5>
-                                        <h5>{{__('Author')}} : {{$post->user->name}} </h5>
-                                        <h5 class="flex">{{__('License')}} : @if(!empty($post->user->license)) {{$post->user->license}}@else {{__('This user didn\'t chose a license')}} @endif 
-                                            <a wire:navigate class="hover:text-primary" href="{{route('about.licensing')}}" > 
-                                                <svg class="ml-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
-                                                    <path d="M478-240q21 0 35.5-14.5T528-290q0-21-14.5-35.5T478-340q-21 0-35.5 14.5T428-290q0 21 14.5 35.5T478-240Zm2 160q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Zm4-172q25 0 43.5 16t18.5 40q0 22-13.5 39T502-525q-23 20-40.5 44T444-427q0 14 10.5 23.5T479-394q15 0 25.5-10t13.5-25q4-21 18-37.5t30-31.5q23-22 39.5-48t16.5-58q0-51-41.5-83.5T484-720q-38 0-72.5 16T359-655q-7 12-4.5 25.5T368-609q14 8 29 5t25-17q11-15 27.5-23t34.5-8Z"/>
-                                                </svg>
-                                            </a>
-                                        </h5>
-                                    </div>
-                                </div>
+                                
+
                                 @if( ($files->contains('type', 'source')) or ($files->contains('type', 'exercise')) or ($files->contains('type', 'exercise correction')))
                                 <div class="pt-6">
                                     <div class="card bg-base-100 shadow-xl">
@@ -296,8 +260,11 @@
                                         </div>
                                     </div>
                                 </div>
-                                @endif
                                 <div class="pt-6">
+                                    @else
+                                    <div class="">
+                                @endif
+                                
                                     <div class="card bg-base-100 shadow-xl">
                                         <div class="card-body">
                                             <h2 class="card-title">{{__('History')}}</h2>
@@ -409,6 +376,8 @@
                                             </div>    
                                     @endif
                                             </div>
+                                            <div class="divider"></div> 
+                                            @if(!empty($post->user->license)) {{__('His/her posts are licensed under')}} {{$post->user->license}}@else {{__('This user didn\'t chose a license for his/her posts')}} @endif 
                                         </div>
                                     </div>
                                 </div>
