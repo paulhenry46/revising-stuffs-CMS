80b2c9156266f04d2f1c4292f7e844ca1a02f60f
paulhenry46 <paulhenrysaux@protonmail.com>
2024-04-10 20:38:54 +0200

    UI refactoring (footer)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,6 +63,8 @@ function editTheme(){
     <x-mary-toast />
         <x-banner />
         <div class="navbar bg-base-100">
+
+        
   <div class="navbar-start">
     <div class="dropdown">
       <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
@@ -101,7 +103,12 @@ function editTheme(){
     </ul>
   </div>
   <div class="navbar-end">
-    
+
+  <label class="cursor-pointer grid place-items-center">
+  <input onclick="editTheme()" type="checkbox" class="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"/>
+  <svg class="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
+  <svg class="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+</label>
   </div>
 </div>
 
@@ -127,17 +134,20 @@ function editTheme(){
 
         @livewireScripts
     </body>
-    <footer class="footer footer-center p-10 bg-neutral text-neutral-content">
-        <aside>
-            <button onclick="editTheme()" class="btn btn-primary">{{__('Change theme')}}</button>
-            <x-application-mark class="block h-9 w-auto" />
-          <p class="font-bold">
-            {{env('APP_NAME')}}
-          </p> 
-          <p>Works thanks to <a class="link link-primary" href="https://github.com/paulhenry46/revising-stuffs-CMS">RSCMS</a>, under MIT license</p>
-          <p>{{__('Version')}} {{env('APP_VERSION')}}</p>
-        </aside> 
-        
-      </footer>
+<footer class="footer footer-center p-10 bg-base-200 text-base-content rounded">
+<x-application-mark />
+<nav class="">
+    <p>{{env('APP_NAME')}}</p>
+    <p>{{__('Version')}} {{env('APP_VERSION')}}</p></br>
+    <div class="grid grid-flow-col gap-4">
+    <a wire:navigate href="{{route('about.about')}}" class="link link-hover">{{__('About us')}}</a>
+    <a wire:navigate href="{{route('about.licensing')}}" class="link link-hover">{{__('License')}}</a>
+    <a class="link link-hover">{{__('Term of Use')}}</a>
+</div> 
+</nav>
+  <aside>
+    <p>{{__('Works thanks to <')}}a class="link link-primary link-hover" href="https://github.com/paulhenry46/revising-stuffs-CMS">RSCMS</a>{{__(', under MIT license')}}</p>
+  </aside>
+</footer>
       
 </html>
