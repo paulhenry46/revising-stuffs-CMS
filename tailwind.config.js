4be23e54ca68226785a583d1fae0f96cdd3f0de9
paulhenry46 <paulhenrysaux@protonmail.com>
2024-10-27 09:45:44 +0100

    bugfix mary css don't install in production

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ export default {
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./vendor/robsontenorio/mary/src/View/Components/**/*.php"
     ],
 
     theme: {
