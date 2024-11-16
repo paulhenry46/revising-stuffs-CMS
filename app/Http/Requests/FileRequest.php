adc821ff1143aa8b77d8f31f71954510284f5b54
paulhenry46 <paulhenrysaux@protonmail.com>
2024-11-16 14:00:34 +0100

    Edit file request

--- app/Http/Requests/FileRequest.php
+++ app/Http/Requests/FileRequest.php
@@ -23,24 +23,24 @@ class FileRequest extends FormRequest
     {
         if($this->input('type') == 'source'){
             return [
-            'type' => 'bail|required|min:4',
-            'name' => 'bail|required|min:4',
+            'type' => 'bail|required|min:1',
+            'name' => 'bail|required|min:1',
             'file' => 'required|max:10000|mimes:doc,docx,pdf,tex,odt,ods,csv'
 
         ];
         
     }elseif($this->input('type') == 'card image'){
             return [
-            'type' => 'bail|required|min:4',
-            'name' => 'bail|required|min:4',
+            'type' => 'bail|required|min:1',
+            'name' => 'bail|required|min:1',
             'file' => 'required|max:10000|mimes:svg,png,jpg,jpeg,gif,webp'
 
         ];
         
         }else{
             return [
-            'type' => 'bail|required|min:4',
-            'name' => 'bail|min:4',
+            'type' => 'bail|required|min:1',
+            'name' => 'bail|min:1',
             'file' => 'required|max:10000|mimes:pdf'
 
         ];
