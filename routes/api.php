4c8159f202077e58aa68c38ab362e9bc6ffb88dd
Paulhenry <paulhenrysaux@gmail.com>
2025-01-24 17:02:49 +0100

    Delete unused Telegram controller

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TelegramWebHookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +13,3 @@ use App\Http\Controllers\TelegramWebHookController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
